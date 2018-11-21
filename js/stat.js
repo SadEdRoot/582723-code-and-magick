d25c51b2c0ebfeae0e51df8053d86a60ccc25e88
Eduard <sadrutdinov@tochka.com>
2018-11-21 09:34:45 +0500

    Внес исправления
    Переписал функцию установки цвета для колонки. Удалил пробелы. Сместил счетчик в цикле.

--- js/stat.js
+++ js/stat.js
@@ -18,7 +18,7 @@ var renderCloud = function (ctx, x, y, color) {
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
-  for (var i = 0; i < arr.length; i++) {
+  for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
@@ -27,31 +27,30 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-function getRandomNumber() {
-  var s = Math.floor(Math.random() * 100);
-  return s;
+function setColorBar(name) {
+  if (name === 'Вы') {
+    return '#c00';
+  } else {
+    return 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
+  }
 }
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
   ctx.fillStyle = '#000';
-  ctx. textAlign = 'left';
+  ctx.textAlign = 'left';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 50);
   ctx.fillText('Список результатов:', 120, 70);
 
   var maxTime = getMaxElement(times);
 
-  ctx. textAlign = 'center';
+  ctx.textAlign = 'center';
   for (var i = 0; i < names.length; i++) {
     ctx.fillText(names[i], CLOUD_X + FONT_GAP + (BAR_WIDCH + GAP) * i, CLOUD_Y);
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = '#c00';
-    } else {
-      ctx.fillStyle = 'hsl(240, ' + getRandomNumber() + '%, 50%)';
-    }
+    ctx.fillStyle = setColorBar(names[i]);
     ctx.fillRect(CLOUD_X + (GAP + BAR_WIDCH) * i, CLOUD_Y - FONT_HEIGHT, BAR_WIDCH, -(BAR_HEIGHT * times[i] / maxTime));
 
     ctx.fillStyle = '#000';
